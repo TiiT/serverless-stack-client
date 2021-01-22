78ab5e553e9b549e80b6586ea2aee14360d377cc
Sonnel Moreau <sonnelmoreau@Sonnels-MBP.T-mobile.com>
2021-01-22 17:07:51 -0500

    Building our React app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
+import React, { useState, useEffect }  from "react";
+import { useHistory } from "react-router-dom";
+import { onError } from "./libs/errorLib";
+
 import { LinkContainer } from "react-router-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Routes from "./Routes";
+import { Auth } from "aws-amplify";
+import { AppContext } from "./libs/contextLib";
 
 function App() {
-  return (
+const history = useHistory();
+const [isAuthenticated, userHasAuthenticated] = useState(true);
+const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+useEffect(()=>{
+   onLoad();
+}, []);
+
+async function onLoad(){
+  try {
+	await Auth.currentSession();
+        userHasAuthenticated(true);
+
+  } catch(e) {
+    if ( e!== 'No current user'){
+      onError(e);
+    }
+  } 
+  setIsAuthenticating(false);
+}
+async function handleLogout() {
+  await Auth.signOut();
+   
+
+  userHasAuthenticated(false);
+  history.push("/login");
+}
+return (
+  !isAuthenticating && (
     <div className="App container py-3">
       <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
         <LinkContainer to="/">
@@ -15,18 +49,33 @@ function App() {
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Nav activeKey={window.location.pathname}>
-            <LinkContainer to="/signup">
-              <Nav.Link>Signup</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <Nav.Link>Login</Nav.Link>
-            </LinkContainer>
+            {isAuthenticated ? (
+              <>
+                <LinkContainer to="/settings">
+                  <Nav.Link>Settings</Nav.Link>
+                </LinkContainer>
+                <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+              </>
+            ) : (
+              <>
+                <LinkContainer to="/signup">
+                  <Nav.Link>Signup</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                  <Nav.Link>Login</Nav.Link>
+                </LinkContainer>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Routes />
+      <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+        <Routes />
+      </AppContext.Provider>
     </div>
-  );
+  )
+);
+
 }
 
 export default App;
--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const config = {
   s3: {
     REGION: "us-east-1",
-    BUCKET: "sm-notes-api-upload",
+    BUCKET: "sm-notes-app-upload",
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://mj8vgahyh3.execute-api.us-east-1.amazonaws.com/prod/notes",
+    URL: "https://mj8vgahyh3.execute-api.us-east-1.amazonaws.com/prod",
   },
   cognito: {
     REGION: "us-east-1",
@@ -13,6 +13,8 @@ const config = {
     APP_CLIENT_ID: "6bk986e0maco5ik9ebvv3si2uf",
     IDENTITY_POOL_ID: "us-east-1:127b7c9a-8ed7-4c1c-a38c-f1983d2771bf",
   },
+  STRIPE_KEY: "pk_test_51IBTSzDqpsV9yWgQVuOyk3xFacORKO6lkY5uwZWGfdZXoGHxIFak5ar3St8CL2OKg5Qvd44ma6si04HbeA4XaBFM00VYk536rz",
+  MAX_ATTACHMENT_SIZE: 5000000,
 };
 
 export default config;

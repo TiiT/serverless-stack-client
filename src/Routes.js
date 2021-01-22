883055263521926d65fa98a5552eefbbf22a69f8
Sonnel Moreau <sonnelmoreau@Sonnels-MBP.T-mobile.com>
2021-01-22 17:55:32 -0500

    Fix Notfound build error

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
-import NotFound from "./containers/NotFound";
+//import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
